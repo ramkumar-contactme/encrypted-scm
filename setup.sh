51f58ac99c3d9bb88e50d33c2ba2ca9cf085e199
Ramkumar M N <ramkumar.contactme@gmail.com>
2025-02-21 22:06:51 +0530

    adding create key steps

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 !#/bin/bash
 
 sudo apt install gnupg  # Ubuntu/Debian
+
+ssh-keygen -t ed25519 -C "ramkumar.contactme@gmail.com"
+
 gpg --full-generate-key
 gpg --list-keys
 
-gpg --recipient "mail.com" --encrypt file1.sh
+gpg --recipient "ramkumar.contactme.com" --encrypt file1.sh
 gpg --decrypt file1.sh.gpg > file1-decrypt.sh
+
+
+
+gpg --recipient "ramkumar.contactme.com" --encrypt file3.txt
+gpg --decrypt file3.sh.gpg > file3-decrypt.txt
